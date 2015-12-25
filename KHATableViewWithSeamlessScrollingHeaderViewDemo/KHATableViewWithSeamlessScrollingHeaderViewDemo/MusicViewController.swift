61834d1fb758a23c03f9a919b7322d5b7f4bd6eb
Kohei Hayakawa <kohei.hayakawa.5@gmail.com>
2015-12-25 17:22:19 +0900

    first commit

---
+++ KHATableViewWithSeamlessScrollingHeaderViewDemo/KHATableViewWithSeamlessScrollingHeaderViewDemo/MusicViewController.swift
@@ -0,0 +1,9 @@
+//
+//  MusicViewController.swift
+//  KHATableViewWithSeamlessScrollingHeaderViewDemo
+//
+//  Created by Kohei Hayakawa on 12/25/15.
+//  Copyright © 2015 Kohei Hayakawa. All rights reserved.
+//
+
+import Foundation
